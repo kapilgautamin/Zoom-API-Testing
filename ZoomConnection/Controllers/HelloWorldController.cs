4ccd86105f5a4afcfbd66fd61eb2d3c77d8b4a23
Kapil Gautam <kapil.gautam@utdallas.edu>
2020-04-17 12:24:06 -0500

    Initial Commit
    Currently code contains basic stubs for GET and POST request.
    Need to generalize variables, currently things are hardcoded.

---
+++ ZoomConnection/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZoomConnection.Controllers
+{
+    public class HelloWorldController : Controller
+    { 
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //// GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ ZoomConnection/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZoomConnection.Models;
+
+namespace ZoomConnection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            Connection connect = new Connection();
+            //var zoomResponse = await connect.GetMeeting();
+            //return Ok(zoomResponse);
+
+            var createMeetingResponse = await connect.CreateMeeting();
+            return Ok(createMeetingResponse);
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
